316cce88c0e5f6b5bb3358770d11dd2187d6639a
François Rozet <francois.rozet@outlook.com>
2024-03-29 15:50:26 +0100

    🚧 Fit data moments

--- priors/train.py
+++ priors/train.py
@@ -7,9 +7,13 @@ import jax.numpy as jnp
 import optax
 import pickle
 
+from inox.random import PRNG, get_rng
 from jax import Array
 from pathlib import Path
-from typing import Any
+from typing import *
+
+from .linalg import *
+from .score import *
 
 
 def dump_module(module: nn.Module, file: Path):
@@ -88,3 +92,69 @@ class EMA(inox.Namespace):
 
     def average(self, x: Array, y: Array) -> Array:
         return x + self.alpha * (y - x)
+
+
+def fit_moments(
+    features: int,
+    rank: int,
+    A: Callable[[Array], Array],
+    y: Array,
+    sigma_y: Array,
+    epochs: int = 4096,
+    learning_rate: float = 1e-3,
+    epsilon: float = 1e-2,
+    key: Array = None,
+) -> Tuple[Array, DPLR]:
+    r"""Fits :math:`\mu_x` and :math:`\Sigma_x` given pairs :math:`(A, y)`."""
+
+    if key is None:
+        rng = get_rng()
+    else:
+        rng = PRNG(key)
+
+    At = transpose(A, jnp.zeros((len(y), features)))
+
+    mu_x, _ = jax.scipy.sparse.linalg.cg(
+        A=lambda x: jnp.mean(At(A(x)), axis=0),
+        b=jnp.mean(At(y), axis=0),
+    )
+
+    bias = A(mu_x) - y
+
+    def objective(params, z):
+        d, U = params
+
+        sigma_x = DPLR((d ** 2 + epsilon) * jnp.ones(features), U, U.T)
+        sigma_Ax = lambda v: A(sigma_x @ At(v)) + sigma_y * v
+
+        loss = jnp.sum(sigma_Ax(z) ** 2, axis=-1) - 2 * jnp.einsum('...i,...i', bias, sigma_Ax(bias))
+
+        return jnp.mean(loss)
+
+    params = (
+        jnp.ones(()),
+        rng.normal((features, rank)) * 1e-2,
+    )
+
+    optimizer = optax.adam(learning_rate=learning_rate)
+    opt_state = optimizer.init(params)
+
+    def update(state, key):
+        params, opt_state = state
+
+        loss, grads = jax.value_and_grad(objective)(params, jax.random.normal(key, y.shape))
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+
+        return (params, opt_state), loss
+
+    (params, opt_state), losses = jax.lax.scan(
+        f=update,
+        init=(params, opt_state),
+        xs=rng.split(epochs),
+    )
+
+    d, U = params
+    sigma_x = DPLR((d ** 2 + epsilon) * jnp.ones(features), U, U.T)
+
+    return mu_x, sigma_x, losses
